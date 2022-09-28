20dc18b093aae147ad67d5069735501a044ea17d
Kornzk <zk930602@gmail.com>
2022-09-28 20:28:47 +1000

    env file

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -21,7 +21,7 @@ export const authOptions = {
       return session;
     },
   },
-  secret: process.env.JWT_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
 export default NextAuth(authOptions);
